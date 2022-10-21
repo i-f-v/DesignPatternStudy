422c1888332b85dea2109a70f6822f2248446806
i-f-v <64691967+i-f-v@users.noreply.github.com>
2022-10-21 22:25:01 +0800

    单例模式更改

--- src/main/java/creational/singleton/Singleton.java
+++ src/main/java/creational/singleton/Singleton.java
@@ -9,19 +9,36 @@ package creational.singleton;
 public class Singleton {
 
     /**
+     * 用于实现单例的枚举类。
+     *
+     * @author i-f-v
+     */
+    enum SingletonEnum {
+        // 直接通过Singleton.INSTANCE可以获取单例
+        INSTANCE;
+
+        /**
+         * 调用单例的方法。
+         */
+        public void method() {
+            System.out.println("This is a singleton object.");
+        }
+    }
+
+    /**
      * 饿汉式（静态变量）。
      *
      * @author i-f-v
      */
     static class SingletonHungryByStaticVar {
 
-        // 私有构造方法。
-        private SingletonHungryByStaticVar() {
-        }
-
         // 内部类创建实例。
         private final static SingletonHungryByStaticVar SINGLETON_INSTANCE =
                 new SingletonHungryByStaticVar();
+
+        // 私有构造方法。
+        private SingletonHungryByStaticVar() {
+        }
 
         /**
          * 提供公有静态方法获取实例对象。
@@ -40,15 +57,15 @@ public class Singleton {
      */
     static class SingletonHungryByStaticCodeBlock {
 
-        // 私有构造方法。
-        private SingletonHungryByStaticCodeBlock() {
-        }
-
         // 私有静态实例，但未初始化。
         private static final SingletonHungryByStaticCodeBlock SINGLETON_INSTANCE;
 
         static {// 静态代码块创建单例对象。
             SINGLETON_INSTANCE = new SingletonHungryByStaticCodeBlock();
+        }
+
+        // 私有构造方法。
+        private SingletonHungryByStaticCodeBlock() {
         }
 
         /**
@@ -157,16 +174,6 @@ public class Singleton {
         }
 
         /**
-         * 用于包装单例的内部类。保证线程安全。
-         *
-         * @author i-f-v
-         */
-        static class SingletonWrapper {
-            private final static SingletonStaticInternalClass SINGLETON_INSTANCE =
-                    new SingletonStaticInternalClass();
-        }
-
-        /**
          * 公有方法获取单例。若单例未初始化创建则创建单例对象。
          * <p>
          * 通过静态内部类实现线程安全，且实现了“懒加载”。
@@ -176,22 +183,15 @@ public class Singleton {
         public static SingletonStaticInternalClass getInstance() {
             return SingletonWrapper.SINGLETON_INSTANCE;
         }
-    }
-
-    /**
-     * 用于实现单例的枚举类。
-     *
-     * @author i-f-v
-     */
-    enum SingletonEnum {
-        // 直接通过Singleton.INSTANCE可以获取单例
-        INSTANCE;
 
         /**
-         * 调用单例的方法。
+         * 用于包装单例的内部类。保证线程安全。
+         *
+         * @author i-f-v
          */
-        public void method() {
-            System.out.println("This is a singleton object.");
+        static class SingletonWrapper {
+            private final static SingletonStaticInternalClass SINGLETON_INSTANCE =
+                    new SingletonStaticInternalClass();
         }
     }
 }
